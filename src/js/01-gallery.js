239d9288cba91acc0ee1315821cd6f1b0336f108
sashadubovyi <125696737+sashadubovyi@users.noreply.github.com>
2023-04-28 15:54:28 +0300

    add library

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,30 @@
+// Описаний в документації
+import SimpleLightbox from 'simplelightbox';
+// Додатковий імпорт стилів
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+const ulGallery = document.querySelector('.gallery');
+const addGallary = createGallery(galleryItems);
+
+ulGallery.innerHTML = addGallary;
+
+function createGallery(images) {
+  return images
+    .map(
+      image =>
+        `
+        <li class="gallery__item">
+            <a class="gallery__link" href="${image.original}">
+                <img class="gallery__image" src="${image.preview}" alt="${image.description}" title="${image.description}" />
+            </a>
+        </li>
+        `
+    )
+    .join('');
+}
+
+var lightbox = new SimpleLightbox('.gallery a', { captionDelay: 250 });
